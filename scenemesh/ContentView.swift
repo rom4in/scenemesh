6b6eda52193d1916ec783f4d64a31a9e0bb5a035
rom4in <rom4in@me.com>
2020-11-18 09:51:52 +0000

    Initial Commit

---
+++ scenemesh/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  scenemesh
+//
+//  Created by BigMac on 18/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ scenemesh/scenemeshApp.swift
@@ -0,0 +1,17 @@
+//
+//  scenemeshApp.swift
+//  scenemesh
+//
+//  Created by BigMac on 18/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct scenemeshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
